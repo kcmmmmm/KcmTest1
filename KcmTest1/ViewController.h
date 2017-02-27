7fd5d2fd86d2d55cb382f220fd14d34734fa4274
openobject <openobject@openobjectui-iMac.local>
2017-02-27 14:32:07 +0900

    Initial Commit

---
+++ KcmTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KcmTest1
+//
+//  Created by openobject on 2017. 2. 27..
+//  Copyright © 2017년 kcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KcmTest1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KcmTest1
+//
+//  Created by openobject on 2017. 2. 27..
+//  Copyright © 2017년 kcm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
